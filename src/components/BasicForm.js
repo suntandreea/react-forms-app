e2e6b3295b35caf12067a486879f6d12eaabec30
Andreea <suntandreea@protonmail.com>
2022-07-06 17:01:07 +0300

    3 inputs form; hook with validation type

--- src/components/BasicForm.js
+++ src/components/BasicForm.js
@@ -1,22 +1,69 @@
+import useInput from '../hooks/use-input';
+
 const BasicForm = (props) => {
+
+  const {
+    value: nameValue,
+    isValid: nameIsValid,
+    hasError: nameHasError,
+    inputChangeHandler: nameChangeHandler,
+    inputBlurHandler: nameBlurHandler,
+    reset: resetName
+  } = useInput();
+
+  const {
+    value: mailValue,
+    isValid: mailIsValid,
+    hasError: mailHasError,
+    inputChangeHandler: mailChangeHandler,
+    inputBlurHandler: mailBlurHandler,
+    reset: resetMail
+  } = useInput('email');
+
+  const {
+    value: passValue,
+    isValid: passIsValid,
+    hasError: passHasError,
+    inputChangeHandler: passChangeHandler,
+    inputBlurHandler: passBlurHandler,
+    reset: resetPass
+  } = useInput();
+
+  let formIsValid = nameIsValid && mailIsValid && passIsValid;
+
+  const submitHandler = () => {
+
+    if (!formIsValid) {
+      return;
+    }
+
+    resetName();
+    resetMail();
+    resetPass();
+
+  };
+
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <div className='control-group'>
-        <div className='form-control'>
-          <label htmlFor='name'>First Name</label>
-          <input type='text' id='name' />
+        <div className={`form-control ${nameHasError && 'invalid'}`}>
+          <label htmlFor='name'>Name</label>
+          <input type='text' id='name' value={nameValue} onChange={nameChangeHandler} onBlur={nameBlurHandler}/>
+          {nameHasError && <p className="error-text">Name cannot be empty.</p>}
         </div>
-        <div className='form-control'>
-          <label htmlFor='name'>Last Name</label>
-          <input type='text' id='name' />
+        <div className={`form-control ${mailHasError && 'invalid'}`}>
+          <label htmlFor='mail'>E-Mail Address</label>
+          <input type='email' id='mail' value={mailValue} onChange={mailChangeHandler} onBlur={mailBlurHandler}/>
+          {mailHasError && <p className="error-text">Insert valid email.</p>}
         </div>
       </div>
-      <div className='form-control'>
-        <label htmlFor='name'>E-Mail Address</label>
-        <input type='text' id='name' />
+      <div className={`form-control ${passHasError && 'invalid'}`}>
+        <label htmlFor='pass'>Password</label>
+        <input type='password' id='pass' value={passValue} onChange={passChangeHandler} onBlur={passBlurHandler}/>
+        {passHasError && <p className="error-text">Insert valid password.</p>}
       </div>
       <div className='form-actions'>
-        <button>Submit</button>
+        <button disabled={!formIsValid}>Submit</button>
       </div>
     </form>
   );
--- src/hooks/use-input.js
+++ src/hooks/use-input.js
@@ -1,12 +1,23 @@
 import {useState} from 'react';
 
 
-const useInput = (validateValue) => {
+const useInput = (validationType) => {
 
   const [enteredValue, setEnteredValue] = useState('');
   const [isTouched, setIsTouched] = useState(false);
 
-  const valueIsValid = validateValue(enteredValue);
+  let valueIsValid = false;
+  switch (validationType) {
+    case 'email':
+      valueIsValid = /^[\w-\.+]+@([\w-]+\.)+[\w]{2,12}$/.test(enteredValue);
+      break;
+    case 'password' :
+      valueIsValid = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~])[A-Za-z\d@$!%*?&]{8,}$/.test(enteredValue);
+      break;
+    default:
+      valueIsValid = enteredValue.trim() !== '';
+  }
+
   const hasError = !valueIsValid && isTouched;
 
   const inputChangeHandler = event => {
